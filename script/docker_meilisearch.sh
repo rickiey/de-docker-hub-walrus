3a53c4f50ab69f16e220288c8d34b5bce12d5851
rickiey <ralkaider@gmail.com>
2024-09-13 16:10:43 +0800

    <chore>:add docker script

---
+++ script/docker_meilisearch.sh
@@ -0,0 +1,9 @@
+
+docker pull getmeili/meilisearch:v1.10 ;
+
+
+docker run -it --rm \
+    -p 7700:7700 \
+    -e MEILI_ENV='development' \
+    -v $(pwd)/meili_data:/meili_data \
+    getmeili/meilisearch:v1.10
